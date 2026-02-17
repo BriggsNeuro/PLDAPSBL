8407e603977e260014de26cc3f9dc98e3dbd5c93
Kristina <nielsenlabmbi@gmail.com>
2026-02-17 16:22:37 -0500

    4A part 2

--- +pldapsExperiments/+lesion/+Setup/lesion_dots_setup_P4A.m
+++ +pldapsExperiments/+lesion/+Setup/lesion_dots_setup_P4A.m
@@ -5,7 +5,7 @@ function p = lesion_dots_setup_P4A(p)
 p = pdsDefaultTrialStructureBL(p); 
 
 %% set the trial function: the function that gets called for each frame state
-p.trial.pldaps.trialFunction='pldapsExperiments.lesion.Trials.lesion_dots_trial_P4';
+p.trial.pldaps.trialFunction='pldapsExperiments.lesion.Trials.lesion_dots_trial_P4A';
 
 %% set general parameters
 p.trial.stimulus.forceCorrect = 1;
